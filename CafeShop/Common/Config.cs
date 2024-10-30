c27c8c09b760cfb0b9e42b4d3caa2acf78ff1b2b
dthgiang683 <dthuonggiang2003@gmail.com>
2024-10-31 00:42:50 +0700

    CoffeShop project

---
+++ CafeShop/Common/Config.cs
@@ -0,0 +1,18 @@
+﻿namespace CafeShop.Common
+{
+    public static class Config
+    {
+        public static string Connection()
+        {
+            string conn = "Data Source=DESKTOP-CCEOE8N;Initial Catalog=cafe;User ID=dthg;Password=abc123;Trust Server Certificate=True";
+            return conn;
+        }
+
+        public static string ImageUrl()
+        {
+            string imageUrl = @"https://localhost:44368/images/";
+            return imageUrl;
+        }
+
+    }
+}
